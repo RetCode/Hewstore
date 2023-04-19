1032708bb70b2508a0043a9e57f81aacaae1b0ed
Ny3D <73645829+Ivanovic346@users.noreply.github.com>
2023-04-19 15:35:31 +0300

    adaptive start

--- public/views/index/index.php
+++ public/views/index/index.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="public/css/min/header-min.css">
 	<link rel="stylesheet" href="public/css/min/index-min.css">
+    <link rel="stylesheet" href="public/css/main-adaptive.css">
     <link rel="icon" type="image/x-icon" href="public/img/icon.png">
 	<title>HewStore</title>
 </head>
