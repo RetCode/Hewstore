24bc217f6cae5416232fbb7e0da70fc40a649783
Ny3D <73645829+Ivanovic346@users.noreply.github.com>
2023-04-23 02:55:52 +0300

    adaptive upd

--- public/views/index/pay.php
+++ public/views/index/pay.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/min/header-min.css">
     <link rel="stylesheet" href="public/css/main-adaptive.css">
-	<link rel="stylesheet" href="public/css/payments-final.css">
+	<link rel="stylesheet" href="public/css/min/payments-final-min.css">
     <link rel="icon" type="image/x-icon" href="public/img/icon.png">
     <title>HewStore</title>
 </head>
